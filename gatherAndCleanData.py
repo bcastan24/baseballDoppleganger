670b89c05db3e866675d38b2aff6a2977d25363a
bcastan24 <messi6509@gmail.com>
2025-08-26 16:18:07 -0500

    Gathered and clean data

---
+++ gatherAndCleanData.py
@@ -0,0 +1,14 @@
+from pybaseball import batting_stats
+import pandas as pd
+import os
+
+if os.path.exists("batting.csv") == False:
+    batting = batting_stats(1980, 2002, qual=200)
+    batting.to_csv("batting1.csv", index=False)
+    batting = batting_stats(2003, 2024, qual=200)
+    batting.to_csv("batting2.csv", index=False)
+    df = pd.concat(map(pd.read_csv, ['batting1.csv', 'batting2.csv']), ignore_index=True)
+    dfFiltered = df.groupby("IDfg").filter(lambda x: x.shape[0] > 7)
+    selectedColumns = ["IDfg", "Name", "Season","Age", "OPS", "wRC+", "K%", "WAR", "PA", "BB%", "HR"]
+    dfFiltered = dfFiltered[selectedColumns]
+    dfFiltered.to_csv("batting.csv", index=False)
